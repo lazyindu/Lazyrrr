f27ffd859e86decffc61d5f8463ed7288ffae69b
indu <70087467+lazyindu@users.noreply.github.com>
2023-02-26 12:31:10 +0530

    Add files via upload

--- plugins/lazy_cb.py
+++ plugins/lazy_cb.py
@@ -27,24 +27,23 @@ async def rename(bot,update):
 async def doc(bot, update):
     type = update.data.split("_")[1]
     new_name = update.message.text
-    name = new_name.split(":-")
-    new_filename = name[1]
+    new_filename = new_name.split(":-")[1]
     file_path = f"downloads/{new_filename}"
     # if new_filename:
     #     await bot.send_message(text=f"Name updated: {new_filename}")
     # else:
     #     await bot.send_message(text=f"Name did'nt updatd")
     #     return
-    message = update.message.reply_to_message
-    file = message.document or message.video or message.audio
-    ms = await update.message.edit("⚠️__**Please wait...**__\n__Downloading file to my server...__")
+    # message = update.message.reply_to_message
+    # file = message.document or message.video or message.audio
+    file = update.message.reply_to_message
+    ms = await update.message.edit("\n__Downloading file to my server...__")
     c_time = time.time()
     try:
         path = await bot.download_media(
-            message=file,
-            progress=progress_for_pyrogram,
-            progress_args=("\n⚠️__**Please wait...**__\n\nRenaming in progress...**", ms, c_time)
-        )
+                message=file,
+                progress=progress_for_pyrogram,
+                progress_args=("**__\n\nRenaming in progress...**", ms, c_time))
     except Exception as e:
         await ms.edit(e)
         return 
@@ -52,25 +51,28 @@ async def doc(bot, update):
     dow_file_name = splitpath[1]
     old_file_name =f"downloads/{dow_file_name}"
     # Ensure that the destination directory exists before renaming the file
-    dest_dir = os.path.dirname(file_path)
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
-        os.rename(old_file_name, file_path)
+    # dest_dir = os.path.dirname(file_path)
+    # if not os.path.exists(dest_dir):
+    #     os.makedirs(dest_dir)
+    os.rename(old_file_name, file_path)
     duration = 0
-    metadata = extractMetadata(createParser(file_path))
-    if metadata.has("duration"):
-        duration = metadata.get('duration').seconds
-    
+    try:
+        metadata = extractMetadata(createParser(file_path))
+        if metadata.has("duration"):
+           duration = metadata.get('duration').seconds
+    except:
+        pass
     user_id = int(update.message.chat.id) 
     ph_path = None 
     media = getattr(file, file.media.value)
     c_caption = await db.get_caption(update.message.chat.id)
     c_thumb = await db.get_thumbnail(update.message.from_user.id)
     if c_caption:
-        vid_list = ["filename", "filesize", "duration"]
-        new_tex = escape_invalid_curly_brackets(c_caption, vid_list)
-        caption = new_tex.format(filename=new_filename, filesize=humanbytes(
-            file.file_size), duration=timedelta(seconds=duration))
+         try:
+             caption = c_caption.format(filename=new_filename, filesize=humanize.naturalsize(media.file_size), duration=convert(duration))
+         except Exception as e:
+             await ms.edit(text=f"Your caption Error unexpected keyword ●> ({e})")
+             return 
     else:
         caption = f"**{new_filename}**"
     if (media.thumbs or c_thumb):
@@ -78,25 +80,16 @@ async def doc(bot, update):
            ph_path = await bot.download_media(c_thumb) 
         else:
            ph_path = await bot.download_media(media.thumbs[0].file_id)
-           Image.open(ph_path).convert("RGB").save(ph_path)
-           img = Image.open(ph_path)
-           img.resize((320, 320))
-           img.save(ph_path, "JPEG")
-           c_time = time.time()
-    else:
-        try:
-            ph_path_ = await take_screen_shot(file_path, os.path.dirname(os.path.abspath(file_path)), random.randint(0, duration - 1))
-            width, height, ph_path = await fix_thumb(ph_path_)
-        except Exception as e:
-            ph_path = None
-            print(e)
-
-    await ms.edit("⚠️__**Please wait...**__\n__Processing file upload....__")
+        Image.open(ph_path).convert("RGB").save(ph_path)
+        img = Image.open(ph_path)
+        img.resize((320, 320))
+        img.save(ph_path, "JPEG")
+    await ms.edit("𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝚄𝙿𝙻𝙾𝙰𝙳𝙸𝙽𝙶....")
     c_time = time.time() 
     try:
        if type == "document":
           await bot.send_document(
-	    update.message.chat.id,
+	        update.message.chat.id,
                    document=file_path,
                    thumb=ph_path, 
                    caption=caption, 
@@ -104,22 +97,22 @@ async def doc(bot, update):
                    progress_args=( "⚠️__**Please wait...**__\n__Processing file upload....__",  ms, c_time   ))
        elif type == "video": 
            await bot.send_video(
-	    update.message.chat.id,
-	    video=file_path,
-	    caption=caption,
-	    thumb=ph_path,
-	    duration=duration,
-	    progress=progress_for_pyrogram,
-	    progress_args=( "⚠️__**Please wait...**__\n__Processing file upload....__",  ms, c_time))
+	        update.message.chat.id,
+	        video=file_path,
+	        caption=caption,
+	        thumb=ph_path,
+	        duration=duration,
+	        progress=progress_for_pyrogram,
+	        progress_args=( "⚠️__**Please wait...**__\n__Processing file upload....__",  ms, c_time))
        elif type == "audio": 
            await bot.send_audio(
-	    update.message.chat.id,
-	    audio=file_path,
-	    caption=caption,
-	    thumb=ph_path,
-	    duration=duration,
-	    progress=progress_for_pyrogram,
-	    progress_args=( "⚠️__**Please wait...**__\n__Processing file upload....__",  ms, c_time   )) 
+	        update.message.chat.id,
+	        audio=file_path,
+	        caption=caption,
+	        thumb=ph_path,
+	        duration=duration,
+	        progress=progress_for_pyrogram,
+	        progress_args=( "⚠️__**Please wait...**__\n__Processing file upload....__",  ms, c_time   )) 
     except Exception as e: 
         await ms.edit(f" Erro {e}") 
         os.remove(file_path)
